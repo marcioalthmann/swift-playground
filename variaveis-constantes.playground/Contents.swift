113f472bbb2e77db94c35ba9eec8eb74273d37f4
Márcio Fábio Althmann <Althmann@skynet.local>
2017-02-06 20:08:30 -0200

    Variáveis e constantes

---
+++ variaveis-constantes.playground/Contents.swift
@@ -0,0 +1,29 @@
+// let para a criação de constantes
+let empresaResponsavel = "Apple"
+
+// Tipo da variável definido implicitamente
+var linguagem = "Swift"
+var anoDeLancamento = 2014
+
+// Tipo da variável definido explicitamente
+var extensao: String = ".swift"
+var versaoAtual: Double = 3.0
+
+
+// Definindo variáveis do mesmo tipo em uma só linha
+var versaoParaMac, versaoParaLinux, versaoParaWindows: Bool
+
+// Definindo multiplas variáveis na mesma linha
+var primeiraVersao = 2014, segundaVersao = 2015, terceiraVersao = 2016, criador = "Chris Lattner"
+
+primeiraVersao
+segundaVersao
+terceiraVersao
+criador
+
+// ; só é necessário para mais de uma declaração na mesma linha
+versaoParaMac = true; versaoParaLinux = true; versaoParaWindows = false
+
+versaoParaMac
+versaoParaLinux
+versaoParaWindows
